2c462491453ba9c80435109baa537bb66349c6d6
Joseph-Cha <joseph.c@kakao.com>
2021-01-29 15:14:34 +0900

    Setup

---
+++ SetUp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class test
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Hello, World");
+    }
+
+    public void Alpha()
+    {
+        
+    }
+
+}
